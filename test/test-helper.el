160bf788cf32a3071bad9f7a0ee3a270a92add8a
Robert Irelan <rirelan@gmail.com>
2022-07-15 15:56:46 -0700

    Test helper to print each test name before running

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,3 +1,8 @@
 ;;; test-helper.el --- Helpers for org-gcal.el-test.el
-
+(defun my-ert-runner-print-test-name (_stats test)
+  (message "ert-runner: now running test %s\n"
+           (ert-test-name test)))
+(when (boundp 'ert-runner-reporter-test-started-functions)
+  (add-to-list 'ert-runner-reporter-test-started-functions
+               'my-ert-runner-print-test-name))
 ;;; test-helper.el ends here
